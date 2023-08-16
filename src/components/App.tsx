9f0886ae70deec2ac7aee79a1ff056a6c23f9ef6
Your Name <you@example.com>
2023-08-16 18:13:58 +0700

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import ShareBio from "view/home/ShareBio";
+
+
+
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-violet-500 to-fuchsia-500 h-screen">
+       <ShareBio></ShareBio>
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/view/home/ShareBio.tsx
@@ -0,0 +1,9 @@
+const ShareBio = () => {
+    return (
+        <div className="container mx-auto columns-xs bg-slate-300 h-80 drop-shadow-lg rounded-b-lg flex  justify-center">
+            <img src="/image/avt.jpg" alt="avatar" className="w-36	h-36 mt-5" />
+        </div>
+    )
+  };
+  
+  export default ShareBio;
